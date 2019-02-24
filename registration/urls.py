a851523fd8b0a76fa4ddb3723c2ee71d99269f58
ebk admin <admin@eigenbaukombinat.de>
2019-02-24 22:39:58 +0100

    serve register page on root instead of relying on frontend server to forward to index.html automagically...

--- registration/urls.py
+++ registration/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index.html$', views.register, name='register'),
+    url(r'^$', views.register, name='register'),
     url(r'^preview/?$', views.preview, name='preview'),
 ]
